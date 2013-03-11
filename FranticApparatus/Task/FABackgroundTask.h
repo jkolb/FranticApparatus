e096d44d885ee0ed423b27147d36b76a3fbf6a78
jkolb <franticapparatus@gmail.com>
2013-03-11 09:18:25 -0500

    On second thought don't relax the type, this parameter exists mostly for checking for early cancelation while generating the result.

--- FranticApparatus/Task/FABackgroundTask.h
+++ FranticApparatus/Task/FABackgroundTask.h
@@ -42,7 +42,7 @@ typedef NS_ENUM(NSInteger, FABackgroundTaskPriority) {
 
 @property (nonatomic) FABackgroundTaskPriority priority;
 
-@property (copy) id (^generateResult)(id blockTask, id parameter, NSError **error);
+@property (copy) id (^generateResult)(id <FATask> blockTask, id parameter, NSError **error);
 
 - (id)generateResultWithParameter:(id)parameter error:(NSError **)error;
 
